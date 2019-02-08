3b62d40b8d734e01685f334e923403904ca056a3
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-08 17:44:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -124,6 +124,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages].push("Assembly")
-programmer_hash
+        programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+        programmer_hash
 end
