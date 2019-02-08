79f2bba538fe1f94291532ef6e0251840bc629f6
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-08 17:35:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,13 +63,9 @@ def dennis_ritchies_language
      programmer_hash[:dennis_ritchie][:languages][0]
 end
 
-def adding_matz# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+def adding_matz
 
-	programmer_hash = 
+programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
